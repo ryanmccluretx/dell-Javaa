13f2a6964c9d297aa283fc7cf172a8d097e74621
McClure <Ryan_McClure@Dell.com>
2019-01-11 17:48:52 -0600

    some projects

---
+++ RockPaperScissors/src/RockPaperScissors.java
@@ -0,0 +1,66 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+	
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Please input 'Rock', 'Paper', or 'Scissors'");
+		String userMove = reader.next();
+		
+		String computerMove = getComputerMove();
+		System.out.println("You played: " + userMove + ". And I played " + computerMove + ".");
+		
+		if(userMove == computerMove) {
+			System.out.println("Oh no! We have a tie.");
+		}
+		else {	
+			String winner = compute(userMove.toUpperCase(), computerMove.toUpperCase());
+			System.out.println(winner + " WINS!");
+		}
+				
+		reader.close();
+	}
+		
+	private static String getComputerMove() {
+		Random generator = new Random(); 
+		int play = generator.nextInt(3);
+		
+		switch(play) {
+		case 0:
+			return "Rock";
+		case 1:
+			return "Paper";
+		case 2:
+			return "Scissors";
+        default: 
+        	return "Ha, Whoops!";		
+		}
+	}
+
+	public static String compute(String userMove, String computerMove) {
+		if(userMove.equals("ROCK") || computerMove.equals("ROCK")) {
+			if(userMove.equals("PAPER") || computerMove.equals("PAPER")) {
+				return "PAPER";
+			}else if(userMove.equals("SCISSORS") || computerMove.equals("SCISSORS")) {
+				return "ROCK";
+			} 
+		}
+		if(userMove.equals("PAPER") || computerMove.equals("PAPER")) {
+			if(userMove.equals("ROCK") || computerMove.equals("ROCK")) {
+				return "PAPER";
+			}else if(userMove.equals("SCISSORS") || computerMove.equals("SCISSORS")) {
+				return "SCISSORS";
+			} 
+		}
+		if(userMove.equals("SCISSORS") || computerMove.equals("SCISSORS")) {
+			if(userMove.equals("ROCK") || computerMove.equals("ROCK")) {
+				return "ROCK";
+			}else if(userMove.equals("PAPER") || computerMove.equals("PAPER")) {
+				return "SCISSORS";
+			} 
+		}
+		return "oops";
+	}
+}
