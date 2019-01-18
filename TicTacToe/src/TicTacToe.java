029005cd4f2634afddabd852fe0934f672a8456d
McClure <Ryan_McClure@Dell.com>
2019-01-18 15:39:14 -0600

    Now it's working really well

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,8 +8,8 @@ public class TicTacToe {
 	static String scoreBoard[][] = new String[3][3] ;
 
 	public static void main(String[] args) {
-
-		Boolean humanTurn;
+		
+		Scanner scanner = new Scanner(System.in);
 		
 		for(int i =0; i < 3; i++) {
 			for(int j=0; j < 3; j++) {
@@ -23,25 +21,27 @@ public class TicTacToe {
 		
 		System.out.println("Let's play Tic-Tac-Toe!");
 		
-		PickTurns();
+		PickTurns(scanner);
 		
 		while(gameStarted) {
-			GetNextMove();
+			GetNextMove(scanner);
+			CheckForWin();
 			PlayNextMove();
 			CheckForWin();
 		}		
 		
+		scanner.close();
 	}
 	
 	private static void CheckForWin() {
-		if((scoreBoard[0][0].equals(scoreBoard[0][1]) && scoreBoard[0][1].equals(scoreBoard[0][2])) ||
-				(scoreBoard[0][0].equals(scoreBoard[1][1]) && scoreBoard[1][1].equals(scoreBoard[2][2])) ||
-				(scoreBoard[0][0].equals(scoreBoard[1][0]) && scoreBoard[1][0].equals(scoreBoard[2][0])) ||
-				(scoreBoard[0][2].equals(scoreBoard[1][1]) && scoreBoard[1][1].equals(scoreBoard[0][2])) ||
-				(scoreBoard[0][1].equals(scoreBoard[1][1]) && scoreBoard[1][1].equals(scoreBoard[2][1])) ||
-				(scoreBoard[0][2].equals(scoreBoard[1][2]) && scoreBoard[1][2].equals(scoreBoard[2][2])) ||
-				(scoreBoard[1][0].equals(scoreBoard[1][1]) && scoreBoard[1][1].equals(scoreBoard[1][2])) ||
-				(scoreBoard[2][0].equals(scoreBoard[2][1]) && scoreBoard[2][1].equals(scoreBoard[2][2]))) {
+		if((scoreBoard[0][0].equals(scoreBoard[0][1]) && scoreBoard[0][1].equals(scoreBoard[0][2]) && !scoreBoard[0][2].equals(" ")) ||
+				(scoreBoard[0][0].equals(scoreBoard[1][1]) && scoreBoard[1][1].equals(scoreBoard[2][2]) && !scoreBoard[2][2].equals(" ")) ||
+				(scoreBoard[0][0].equals(scoreBoard[1][0]) && scoreBoard[1][0].equals(scoreBoard[2][0]) && !scoreBoard[2][0].equals(" ")) ||
+				(scoreBoard[0][2].equals(scoreBoard[1][1]) && scoreBoard[1][1].equals(scoreBoard[0][2]) && !scoreBoard[0][2].equals(" ")) ||
+				(scoreBoard[0][1].equals(scoreBoard[1][1]) && scoreBoard[1][1].equals(scoreBoard[2][1]) && !scoreBoard[2][1].equals(" ")) ||
+				(scoreBoard[0][2].equals(scoreBoard[1][2]) && scoreBoard[1][2].equals(scoreBoard[2][2]) && !scoreBoard[2][2].equals(" ")) ||
+				(scoreBoard[1][0].equals(scoreBoard[1][1]) && scoreBoard[1][1].equals(scoreBoard[1][2]) && !scoreBoard[1][2].equals(" ")) ||
+				(scoreBoard[2][0].equals(scoreBoard[2][1]) && scoreBoard[2][1].equals(scoreBoard[2][2]) && !scoreBoard[2][2].equals(" "))) {
 			System.out.println("We have a Winner!");
 			gameStarted = false;
 		}		
@@ -68,95 +68,111 @@ public class TicTacToe {
 			computerMove = new Random().nextInt(10);
 		};	
 		
+		System.out.println("Ok, now it's my turn");
+		
 		switch(computerMove) {
 		case 1:
 			scoreBoard[0][0] = humanTeam ? "X" : "O";
+			break;
 		case 2:
 			scoreBoard[0][1] = humanTeam ? "X" : "O";
+			break;
 		case 3:
-			scoreBoard[0][2] = humanTeam ? "X" : "O";        	
+			scoreBoard[0][2] = humanTeam ? "X" : "O";       
+			break;
 		case 4:
-			scoreBoard[1][0] = humanTeam ? "X" : "O";        	
+			scoreBoard[1][0] = humanTeam ? "X" : "O";  
+			break;
 		case 5:
-			scoreBoard[1][1] = humanTeam ? "X" : "O";        	
+			scoreBoard[1][1] = humanTeam ? "X" : "O"; 
+			break;
 		case 6:
-			scoreBoard[1][2] = humanTeam ? "X" : "O";        	
+			scoreBoard[1][2] = humanTeam ? "X" : "O";  
+			break;
 		case 7:
-			scoreBoard[2][0] = humanTeam ? "X" : "O";        	
+			scoreBoard[2][0] = humanTeam ? "X" : "O";
+			break;
 		case 8:
-			scoreBoard[2][1] = humanTeam ? "X" : "O";        	
+			scoreBoard[2][1] = humanTeam ? "X" : "O";
+			break;
 		case 9:
-			scoreBoard[2][2] = humanTeam ? "X" : "O";        	
+			scoreBoard[2][2] = humanTeam ? "X" : "O";
+			break;
 		}	
 		
 		PrintBoard();
 	}
 
-	public static void GetNextMove() {
-		
-		Scanner reader = new Scanner(System.in);
-		
+	public static void GetNextMove(Scanner scanner) {
 		int humanMove = -1;		
 		
 		Boolean validateMove = false;
 		
-		while(!validateMove) {
-			System.out.println("Pick a square!");	
-			humanMove = reader.nextInt();		
-			validateMove = ValidateMove(humanMove);
+		while(!validateMove) {			
+			System.out.println("Pick a square!");		      
+		        humanMove = scanner.nextInt();							
+		  		validateMove = ValidateMove(humanMove);
 		};
 				
 		switch(humanMove) {
 		case 1:
 			scoreBoard[0][0] = humanTeam ? "O" : "X";
+			break;
 		case 2:
 			scoreBoard[0][1] = humanTeam ? "O" : "X";
+			break;
 		case 3:
-			scoreBoard[0][2] = humanTeam ? "O" : "X";        	
+			scoreBoard[0][2] = humanTeam ? "O" : "X";   
+			break;
 		case 4:
-			scoreBoard[1][0] = humanTeam ? "O" : "X";        	
+			scoreBoard[1][0] = humanTeam ? "O" : "X";    
+			break;
 		case 5:
-			scoreBoard[1][1] = humanTeam ? "O" : "X";        	
+			scoreBoard[1][1] = humanTeam ? "O" : "X";  
+			break;
 		case 6:
-			scoreBoard[1][2] = humanTeam ? "O" : "X";        	
+			scoreBoard[1][2] = humanTeam ? "O" : "X";
+			break;
 		case 7:
-			scoreBoard[2][0] = humanTeam ? "O" : "X";        	
+			scoreBoard[2][0] = humanTeam ? "O" : "X";   
+			break;
 		case 8:
-			scoreBoard[2][1] = humanTeam ? "O" : "X";        	
+			scoreBoard[2][1] = humanTeam ? "O" : "X"; 
+			break;
 		case 9:
-			scoreBoard[2][2] = humanTeam ? "O" : "X";        	
+			scoreBoard[2][2] = humanTeam ? "O" : "X";
+			break;
+		default:
+			break;
 		}			
 		
 		PrintBoard();
 		
-		reader.close();	
 		
 	}
 
 	static void PrintBoard() {
 		
 		System.out.println("1    |2    |3    ");
-		System.out.println("  "+"scoreBoard[0][0]"+"  |  "+"scoreBoard[0][1]"+"  |  "+"scoreBoard[0][2]"+"  ");
+		System.out.println("  "+scoreBoard[0][0]+"  |  "+scoreBoard[0][1]+"  |  "+scoreBoard[0][2]+"  ");
 		System.out.println("_____|_____|_____");
 		System.out.println("4    |5    |6    ");
-		System.out.println("  "+"scoreBoard[1][0]"+"  |  "+"scoreBoard[1][1]"+"  |  "+"scoreBoard[1][2]"+"  ");
+		System.out.println("  "+scoreBoard[1][0]+"  |  "+scoreBoard[1][1]+"  |  "+scoreBoard[1][2]+"  ");
 		System.out.println("_____|_____|_____");		
 		System.out.println("7    |8    |9    ");
-		System.out.println("  "+"scoreBoard[2][0]"+"  |  "+"scoreBoard[2][1]"+"  |  "+"scoreBoard[2][2]"+"  ");
+		System.out.println("  "+scoreBoard[2][0]+"  |  "+scoreBoard[2][1]+"  |  "+scoreBoard[2][2]+"  ");
 		System.out.println("     |     |     ");
 	}
 	
-	public static void PickTurns() {
+	public static void PickTurns(Scanner scanner) {
 	
-		Scanner reader = new Scanner(System.in);
-
 		String humanMove = null;
 		
 		Boolean validInput = false;
 		
 		while(!validInput) {
 			System.out.println("Do you want to be Xs or Os ??");	
-			humanMove = reader.next();		
+			humanMove = scanner.next();		
 			validInput = ValidateTurnInput(humanMove);
 		};		
 		
@@ -168,7 +184,6 @@ public class TicTacToe {
 		
 		gameStarted = true;
 		
-		reader.close();	
 	}
 
 	public static Boolean ValidateTurnInput(String humanMove) {
@@ -190,38 +205,47 @@ public class TicTacToe {
 				if(scoreBoard[0][0] != " ") {
 					return false;
 				}
+				break;
 			case 2:
 				if(scoreBoard[0][1] != " ") {
 					return false;
 				}
+				break;
 			case 3:				  
 				if(scoreBoard[0][2] != " ") {
 					return false;
 				}
+				break;
 			case 4:				        	
 				if(scoreBoard[1][0] != " ") {
 					return false;
 				}
+				break;
 			case 5:
 				if(scoreBoard[1][1] != " ") {
 					return false;
 				}
+				break;
 			case 6:				    
 				if(scoreBoard[1][2] != " ") {
 					return false;
 				}
+				break;
 			case 7:				  
 				if(scoreBoard[2][0] != " ") {
 					return false;
 				}
+				break;
 			case 8:
 				if(scoreBoard[2][1] != " ") {
 					return false;
 				}
+				break;
 			case 9:
 				if(scoreBoard[2][2] != " ") {
 					return false;
 				}
+				break;
 			}
 			return true;
 		}		
